6abb50aa916859221bbd4a8ed4a4e15ca3151244
Renato Freitas <natogfreitas@gmail.com>
2023-06-02 13:54:32 -0300

    pokedex in angular

---
+++ src/app/service/poke-api.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+//Observable
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeApiService {
+
+  private url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=100';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get apiListAllPokemons(): Observable<any> {
+    return this
+      .http
+      .get<any>(this.url)
+      .pipe(
+        // tap(res => {
+        //   console.log("RES1", res);
+        // }),
+        tap(res => {
+          res.results.map((resPokemons: any) => {
+
+            this.apiGetPokemon(resPokemons.url)
+              .subscribe(res => resPokemons.status = res)
+          });
+
+        })
+      )
+  }
+
+  public apiGetPokemon(url: string): Observable<any> {
+    return this
+      .http
+      .get<any>(url)
+      .pipe(
+        map(res => res)
+      );
+  }
+}
